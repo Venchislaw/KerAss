0d4ef041d0fcabd454e784efbb5a83694d1c94cf
Venchislaw <venchislavcodes@gmail.com>
2024-09-23 21:38:18 +0300

    Stary Bog

---
+++ activations.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def relu(z):
+    return np.maximum(0, z)
+
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+def softmax(z):
+    return np.exp(z) / np.sum(np.exp(z))
+
+activations_map = {
+    "linear": lambda z: z,
+    "relu": relu,
+    "sigmoid": sigmoid,
+    "softmax": softmax,
+}
--- layers.py
+++ layers.py
@@ -1,2 +1,27 @@
+import numpy as np
+from activations import activations_map
+
 class Dense:
-    def __init__(self)
+    def __init__(self, n_neurons, activation="linear"):
+        self.n_neurons = n_neurons
+        self.activation = activation.strip().lower()
+
+        self.weights = None
+        self.bias = np.zeros((n_neurons, 1))
+        self.X = None
+
+
+    def forward_prop(self, X):
+        if not self.weights:
+            n_samples, n_features = X.shape
+            self.weights = np.random.randn(self.n_neurons, n_features)
+        self.X = X.T
+
+        z = np.dot(self.weights, self.X) + self.bias
+        a = activations_map[self.activation](z)
+        return a
+
+
+dense_layer = Dense(10, "ReLU")
+res = dense_layer.forward_prop(np.random.randn(1_000, 30))
+print(res.min())  # 0.0
