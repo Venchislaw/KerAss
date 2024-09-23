b03d1df8104b83e9708cef3219cdbfd86a4e9dae
Venchislaw <venchislavcodes@gmail.com>
2024-09-23 22:19:05 +0300

    Fixed bs

--- layers.py
+++ layers.py
@@ -1,4 +1,5 @@
 import numpy as np
+
 from activations import activations_map, diff_act_map
 
 class Dense:
@@ -26,20 +27,23 @@ class Dense:
         if not last:
             da = np.dot(output_grad, previous_w)
             dz = da * diff_act_map[self.activation](self.z)
-            dw = np.dot(dz, self.X.T)
         else:
             da = Y - self.a
             dz = da * diff_act_map[self.activation](self.z)
-            dw = np.dot(dz, self.X.T)
 
-        # update
+        dw = np.dot(dz, self.X.T)  # Calculate gradient of weights correctly
+        db = np.sum(dz, axis=1, keepdims=True)  # Correct gradient calculation for biases
+
+        # Update weights and biases
         self.weights -= learning_rate * dw
-        self.bias -= learning_rate * np.sum(output_grad)
+        self.bias -= learning_rate * db
 
-        return output_grad
+        return dz, self.weights
 
 dense_layer = Dense(10, "ReLU")
 res = dense_layer.forward(np.random.randn(1_000, 30))
 print(res.min())  # 0.0
-grad = dense_layer.backward(np.random.randn(10, 1_000), last=True, Y=np.random.randn(10, 1_000))
-print(grad)
+tmp = dense_layer.weights.copy()
+grad, w = dense_layer.backward(np.random.randn(10, 1_000), last=True, Y=np.random.randn(10, 1_000))
+
+print(tmp[0][0], dense_layer.weights[0][0])
